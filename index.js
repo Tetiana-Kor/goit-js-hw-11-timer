ad0519b19fd9ce1b2a422c184a140dfc027215b6
Tetiana-Kor <63547594+Tetiana-Kor@users.noreply.github.com>
2020-10-26 15:23:46 +0200

    Готовий таймер

--- index.js
+++ index.js
@@ -1,29 +1,81 @@
-new CountdownTimer({
+refs = {
+  daysEl: document.querySelector('span[data-value="days"]'),
+  hoursEl: document.querySelector('span[data-value="hours"]'),
+  minsEl: document.querySelector('span[data-value="mins"]'),
+  secsEl: document.querySelector('span[data-value="secs"]'),
+};
+
+class CountdownTimer {
+  constructor({ targetDate }) {
+    this.intervalId = null;
+    this.targetDate = targetDate;
+    this.start();
+    this.init();
+  }
+
+  init() {
+    const time = this.getTimeComponents(0);
+    this.updateTime(time);
+  }
+
+  start() {
+    this.intervalId = setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = this.targetDate - currentTime;
+      const time = this.getTimeComponents(deltaTime);
+      this.updateTime(time);
+    }, 1000);
+  }
+
+  updateTime({ days, hours, mins, secs }) {
+    refs.daysEl.textContent = `${days}`;
+    refs.hoursEl.textContent = `${hours}`;
+    refs.minsEl.textContent = `${mins}`;
+    refs.secsEl.textContent = `${secs}`;
+  }
+
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+
+  getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { days, hours, mins, secs };
+  }
+}
+
+const timer = new CountdownTimer({
   selector: '#timer-1',
-  targetDate: new Date('Jul 17, 2019'),
+  targetDate: new Date('Nov 15, 2020 11:36 AM'),
 });
 
 /*
  * Дні, що залишилися: ділимо значення UTC на 1000 * 60 * 60 * 24, кількість
  * мілісекунд в один день (мілісекунди * секунди * хвилини * години)
  */
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
+// const days = Math.floor(time / (1000 * 60 * 60 * 24));
 
 /*
  * Решта годин: отримуємо залишок від попереднього розрахунку за допомогою оператора
  * залишку% і ділимо його на кількість мілісекунд в одній годині
  * (1000 * 60 * 60 = мілісекунди * хвилини * секунди)
  */
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+// const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 
 /*
  * Решта хвилин: отримуємо хвилини, що залишилися і ділимо їх на кількість
  * мілісекунд в одній хвилині (1000 * 60 = мілісекунди * секунди)
  */
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+// const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
 
 /*
  * Решта секунд: отримуємо секунди, які залишилися і ділимо їх на кількість
  * миллисекунд в одной секунде (1000)
  */
-const secs = Math.floor((time % (1000 * 60)) / 1000);
+// const secs = Math.floor((time % (1000 * 60)) / 1000);
